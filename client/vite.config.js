0c3fb201c7f843fc55ed5b11da141cffe859b6a1
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-07-29 14:13:00 +0300

    Modifed build to run without installing in ./client. added install step to workflow.

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './client',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  },
   plugins: [react()],
   server: {
     proxy: {
